c49220e7c2bec3a4be71e8e4124e3a4251ff39e1
hacker23 <70826135+haker23@users.noreply.github.com>
2022-01-02 14:48:19 +0800

    Update AdminController.java

--- fan/controller/AdminController.java
+++ fan/controller/AdminController.java
@@ -34,48 +34,48 @@ public class AdminController {
     public R StoreInfo() { return adminService.getAllStoreInfo(); }
 
     @PostMapping("/storesInfo/update")
-    public R modifyStore(@RequestBody StoreForm storeForm) { return adminService.updateStore(storeForm); }
+    public R modifyStore(StoreForm storeForm) { return adminService.updateStore(storeForm); }
 
     @PostMapping("/storesInfo/delete")
     public R deleteStore(@RequestParam("storeId") Long storeId) { return adminService.removeStore(storeId); }
 
     @PostMapping("/storesInfo/insert")
-    public R insertStore(@RequestBody StoreForm storeForm) { return adminService.addStore(storeForm); }
+    public R insertStore(StoreForm storeForm) { return adminService.addStore(storeForm); }
 
     @GetMapping("/storesInfo/goods")
     public R GoodsInStore(@RequestParam("storeId") Long storeId) { return adminService.getAllGoodsInEachStore(storeId);}
 
     @PostMapping("/storesInfo/goods/update")
-    public R modifyGood(@RequestBody GoodForm goodForm) { return adminService.updateGood(goodForm); }
+    public R modifyGood(GoodForm goodForm) { return adminService.updateGood(goodForm); }
 
     @PostMapping("storesInfo/goods/delete")
     public R deleteGood(@RequestParam("goodsId") Long goodsId) { return adminService.removeGood(goodsId); }
 
     @PostMapping("storesInfo/goods/insert")
-    public R insertGood(@RequestBody GoodForm goodForm) { return adminService.addGood(goodForm); }
+    public R insertGood(GoodForm goodForm) { return adminService.addGood(goodForm); }
 
     @GetMapping("/info")
     public R AdminInfo(@RequestParam("adminId") Long adminId) { return adminService.getAdminInfo(adminId); }
 
     @PostMapping("/usersInfo/update")
-    public R modifyUserInfo(@RequestBody UserForm userForm) { return adminService.updateUserInfo(userForm); }
+    public R modifyUserInfo(UserForm userForm) { return adminService.updateUserInfo(userForm); }
 
     @PostMapping("/usersInfo/delete")
     public R deleteUser(@RequestParam("userId") Long userId) { return adminService.removeUser(userId); }
 
     @PostMapping("/usersInfo/insert")
-    public R insertUser(@RequestBody UserForm userForm) { return adminService.addUser(userForm); }
+    public R insertUser(UserForm userForm) { return adminService.addUser(userForm); }
 
     @GetMapping("/ordersInfo")
     public R getAllOrders() { return adminService.getAllOrderInfo(); }
 
     @PostMapping("/ordersInfo/update")
-    public R modifyOrder(@RequestBody OrderForm orderForm) { return adminService.updateOrder(orderForm); }
+    public R modifyOrder(OrderForm orderForm) { return adminService.updateOrder(orderForm); }
 
     @PostMapping("/ordersInfo/delete")
     public R deleteOrder(@RequestParam("orderItemId") Long orderItemId) { return adminService.removeOrder(orderItemId); }
 
     @PostMapping("/ordersInfo/insert")
-    public R insertOrder(@RequestBody OrderForm orderForm) { return adminService.addOrder(orderForm); }
+    public R insertOrder(OrderForm orderForm) { return adminService.addOrder(orderForm); }
 }
 
